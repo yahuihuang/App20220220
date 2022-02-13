de6394a80afa8da8c9b12fb8653835a77406a540
Huang Grace <huang.yahui.grace@gmail.com>
2022-02-13 17:27:27 +0800

    init

--- App20220220/ViewController.swift
+++ App20220220/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let path = Bundle.main.path(forResource: "stock-market", ofType: "png") {
+            print(path)
+            theImage.image = UIImage(contentsOfFile: path)
+        }
     }
 
 
