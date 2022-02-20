779a5c7913c8fc8040ecaa168ecea586ef7ff466
Huang Grace <huang.yahui.grace@gmail.com>
2022-02-20 12:02:19 +0800

    調整 UISegmentedControl 元件的字型

--- App20220220/ViewController.swift
+++ App20220220/ViewController.swift
@@ -11,9 +11,12 @@ class ViewController: UIViewController, UICollectionViewDataSource {
 
     @IBOutlet weak var theCollectionView: UICollectionView!
     var images:[UIImage?] = []
+    @IBOutlet weak var mySegment: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        /*https://medium.com/%E5%BD%BC%E5%BE%97%E6%BD%98%E7%9A%84-swift-ios-app-%E9%96%8B%E7%99%BC%E5%95%8F%E9%A1%8C%E8%A7%A3%E7%AD%94%E9%9B%86/%E8%AA%BF%E6%95%B4-uisegmentedcontrol-%E5%85%83%E4%BB%B6%E7%9A%84%E5%AD%97%E5%9E%8B-ed6995018f65*/
+        mySegment.setTitleTextAttributes([.font: UIFont.systemFont(ofSize: 26.0)], for: .normal)
         
         images = [UIImage(named: "image1"),
                   UIImage(named: "image2"),
