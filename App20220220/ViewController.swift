de03b8df09c3b050f08fa471b8c418882841a3b9
Huang Grace <huang.yahui.grace@gmail.com>
2022-02-20 10:40:37 +0800

    設定好collectionView

--- App20220220/ViewController.swift
+++ App20220220/ViewController.swift
@@ -7,20 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource {
 
-    @IBOutlet weak var theImage: UIImageView!
+    
+    @IBOutlet weak var theCollectionView: UICollectionView!
+    var images:[UIImage?] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        if let path = Bundle.main.path(forResource: "stock-market", ofType: "png") {
-//            print(path)
-//            theImage.image = UIImage(contentsOfFile: path)
-//        }
-//    }
-        theImage.image = UIImage(named: "stock-market")
-        view.backgroundColor = UIColor(named: "myred")
+        images = [UIImage(named: "AppIcon-1"),
+                  UIImage(named: "AppIcon-2"),
+                  UIImage(named: "AppIcon-3"),
+                  UIImage(named: "AppIcon-4"),
+                  UIImage(named: "AppIcon-5"),
+                  UIImage(named: "AppIcon-6")]
+        
+        theCollectionView.dataSource = self
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath)
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+    
 }
 
